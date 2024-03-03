009fb633cc4ff32af1d1568064190eb9f737fc08
Ellie Xing <elliexin@usc.edu>
2024-03-02 21:54:43 -0800

    Edit Preferences page frontend done

--- site/src/App.js
+++ site/src/App.js
@@ -3,6 +3,7 @@ import {Navigate, Route, Routes} from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
+import Preferences from "./pages/Preferences/Preferences";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/" element={<Home />}/>
                 <Route path="/login" element={<Login />}/>
                 <Route path="/signup" element={<SignUp />}/>
+                <Route path="/preferences" element={<Preferences />}/>
                 <Route path="*" element={<Navigate to="/" replace />}/>
             </Routes>
         </div>
